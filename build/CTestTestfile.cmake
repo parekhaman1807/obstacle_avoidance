90a2be29eb7ecdedff10073340069bc3052f6199
Aman Parekh <amanpar@iitk.ac.in>
2018-11-30 22:47:53 +0530

    Initial Commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/aman/obstacle_avoidance/src
+# Build directory: /home/aman/obstacle_avoidance/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(obs)
---
+++ build/obs/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/aman/obstacle_avoidance/src/obs
+# Build directory: /home/aman/obstacle_avoidance/build/obs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
