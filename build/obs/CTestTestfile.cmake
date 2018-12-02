44c009a356754949865b6fada35599cc8f7ca563
gajen <gajena@iitk.ac.in>
2018-12-02 18:59:54 +0530

    directories rearrnage

--- build/obs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aman/obstacle_avoidance/src/obs
-# Build directory: /home/aman/obstacle_avoidance/build/obs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
